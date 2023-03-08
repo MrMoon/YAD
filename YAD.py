287a33ff7a17684d6be9069d1bb0f572cccf0a51
BahouA <55353498+BahouA@users.noreply.github.com>
2023-03-09 00:18:11 +0300

    Update YAD.py

--- YAD.py
+++ YAD.py
@@ -1,26 +1,82 @@
 import typer
 import re
 
+
+
 app = typer.Typer()
 
-@app.command("extract")
-def extract_comments(file: str  = typer.Argument(...), output: str  = typer.Argument(...)):
+
+
+@app.command("delete")
+def delete_comments(input: str  = typer.Argument(...), output: str  = typer.Argument(...)):
     """
-    This is a test run for Comment Remover
+    This tool will delete all the comments from a C++ file and output the source code into the file of your choosing.
     """
-    with open(file, 'r') as f:
-        contents = f.read()
+    with open(input, 'r') as f:
+        source = f.read()
     
     # Remove all comments from the input file
-    contents = re.sub(r'\/\/.*?$|\/\*[\s\S]*?\*\/', '', contents, flags=re.MULTILINE)
+    source = re.sub(r'\/\/.*?$|\/\*[\s\S]*?\*\/', '', source, flags=re.MULTILINE)
 
     # Write the modified contents to the output file
     with open(output, 'w') as f:
-        f.write(contents)
+        f.write(source)
 
-@app.command("test")
-def test():
-    print("hello")
+
+
+@app.command("extract")
+def extract_comments(input: str  = typer.Argument(...), output: str  = typer.Argument(...)):
+    """
+    This tool will extract all the comments from a C++ file and output the comments into the file of your choosing.
+    """
+    with open(input, 'r') as f:
+        source = f.read()
+    
+    # Extract all comments from the input file
+    comments = re.findall(r'\/\/.*?$|\/\*[\s\S]*?\*\/', source, flags=re.MULTILINE)
+
+    # Write the comments to the output file
+    with open(output, 'w') as f:
+        for comment in comments:
+            size = len(comment)
+            if comment[0:2] == "/*":
+                comment = comment[2:size-2].strip()
+            else:
+                comment = comment[2:size].strip()
+            f.write(comment + "\n")
+
+
+
+@app.command("header")
+def extract_header(file: str  = typer.Argument(...), output: str  = typer.Argument(...)):
+    """
+    This tool will extract all header comments from a C++ file and output the comments into the file of your choosing.
+    """
+    with open(file, 'r') as f:
+        source = f.read()
+
+    header = re.findall(r'\/\/.*?$|\/\*[\s\S]*?\*\/', source, flags=re.MULTILINE)
+    amount = 0
+
+    while source[0:2] == "//" or source[0:2] == "/*":
+        source = re.sub(r'\/\/.*?$|\/\*[\s\S]*?\*\/', '',source, flags=re.MULTILINE, count=1)
+        amount = amount + 1
+        while source[0:1] == '\n':
+            source = source[1:]
+
+    # Write the header comments to the output file
+    with open(output, 'w') as f:
+        for n in range(amount):
+            comment = header[n]
+            size = len(comment)
+            if comment[0:2] == "/*":
+                comment = comment[2:size-2]
+            else:
+                comment = comment[2:size]
+            f.write(comment + "\n")
+
+
+
 
 if __name__ == "__main__":
     app()
