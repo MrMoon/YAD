560d0ba4d44fa46db3a1732eb6e5167030ca87c0
Rund <78150340+RundRasheed@users.noreply.github.com>
2023-04-02 10:50:14 +0300

    updateing codeParser using libclang

--- codeParser.py
+++ codeParser.py
@@ -1,188 +1,179 @@
 import re
 import os
+import orjson, json
+import clang.cindex 
 
+def findLocationFunction(data, prototype: str):
 
+    isTemplate = 0
+    if len(prototype.split("template"))> 1 :
+        isTemplate=1
+        prototype = prototype.split(">")
+        prototype[0] = prototype[0] + ">"
+        regex =  re.split('template\\s*<\\s*typename\\s*([a-zA-Z])\\s*>\\s*',prototype[0])
+        returntype = regex[1]
+        prototype  = prototype[1]
+        params = prototype
+        prototype = prototype.split(returntype)[1]
 
-def findLocationFunction(source: str, fnc: str):
-    #Validate that the function prototype is correct
-    # validFunctionNamePattern = r'^\s*(?:(?:\w+\s+)*\w+\s*::\s*)?(?:\w+\s+)*\w+\s*\(\s*(?:\w+\s+\w+\s*(?:,\s*\w+\s+\w+\s*)*)?\)\s*$'
-    # if re.match(validFunctionNamePattern, fnc) is None:
-    #     print("Function name is not valid syntactically.")
-    #     return
+    name=re.split('\s+|\(', prototype)[1]
+    if isTemplate == 0:
+        returntype = prototype.split(name)[0].strip()
+        
+    if isTemplate == 1:
+        prototype = params
+        
+    params = prototype.split(name)[1].strip()
+    qualType = returntype + params
+    qualType = qualType.replace(" ", "")
+    pos = []
+    type = ""
+    if isTemplate == 0:
+        for item in data['nodes']:
+            if item['kind'] == "FUNCTION_DECL":
+                type  = "function"
+            if item['kind'] == "CXX_METHOD":
+                type = "member_function_outside"
+            if type != "" and item['spelling'].strip() == name.strip() and returntype.strip() == item['prototype'].split("(")[0].strip() and params == "(" + item['prototype'].split("(")[1]:
+                end = item['end']
+                start = item['start']
+                pos += [start, end, "function"]
+    else:
+            flag=0
+            for item in data['nodes']:
+                #not the best way!!
+                if flag == 1 and item['kind'] == "FUNCTION_TEMPLATE" or  item['kind'] == "FUNCTION_DECL" or item['kind'] == "STRUCT_DECL" or item['kind'] == "CLASS_DECL":
+                    end = item['start']-1 
+                    pos+=[end]
+                    break
+                #strip is not enough, find better solution
+                if item['kind'] == "FUNCTION_TEMPLATE" and item['spelling'].strip() == name.strip() and returntype.strip() == item['prototype'].split("(")[0].strip() and params.strip() == "(" + item['prototype'].split("(")[1].strip():
+                    start = item['start']
+                    flag=1
+                    pos += [start]
+            #fix this
+            if flag == 0:
+                pos+=[10000]
+                
+    return pos
 
-    fnc = fnc.strip()
-    #fnc stands for function
-    functionNameHelper = fnc.split('(')[0].split(' ')
-    functionName = functionNameHelper[len(functionNameHelper)-1]
-
-    #System call to get ast dump of anything containing the string in funcName (funcName stands for function name)
-    systemCall = "clang-check -ast-dump -ast-dump-filter={} {} -- 2>&1".format(functionName, source)
-    retrieveSystemCall = os.popen(systemCall).read()
-    if retrieveSystemCall == "":
-        print("Function not found in "+ source)
-        return
+def findLocationClass(data, prototype: str, iter: int):
+    name = prototype.split(" ")[1]
+    pos=[]
+    stc = -1
+    enc = -1
+    for item in data['nodes']:
+        #find start and end positions of a class given its name
+        if item['spelling'] == name and item['is_class'] == True:
+            start = item['start']
+            end = item['end']
+            stc = start 
+            enc = end
+            if iter == 0:
+                pos+=[start, end, "class"]
     
-    #Flag to check if function is part of class (cls = class)
-    isFunctionInClass = False
-    #Flag to check if function is using template
-    isFunctionTemplate = False
-    #Flag to check if function is static
-    isFunctionStatic = False
+        #check member functions implemented outside the class
+        #need to add template  
+        #where to put this?
+        if item['start'] < stc or item['end'] > enc:
+            if item['mangled_name'].startswith('?'):
+                mangledName= item['mangled_name'].split("@")
+                if len(mangledName) >= 1 and mangledName[1] == name:
+                    if item['kind'] == "CXX_METHOD":
+                        returnType = item['prototype'].split(" ")[0]
+                        prototype = returnType +" " +item['displayname']
+                        pos += findLocationFunction(data, prototype)
+                    # start = item['start']
+                    # end = item['end']
+                    # pos+=[start, end, "member_function_outside"]
+                    
+        for inherit in item['inherits_from']:
+            if inherit == name:
+                start = item['start']
+                end = item['end']
+                pos+=[start, end, "inheritance"]
+                pos += findLocationClass(data, "class "+  item['spelling'], iter+1)
+                        
+        #find start and end positions of all classes with friendship with this class
+        for friend in item['friend_with']:
+            if friend == prototype:
+                start = item['start']
+                end = item['end']
+                pos+=[start, end, "friendship"]
+                pos += findLocationClass(data,  "class " + item['spelling'], iter+1)
+    return pos
 
-    #Filter the code using regex to keep what is important (True condition of if statement means that it is only a function not a function inside a class)
-    if fnc[:8] == "template":
-        isFunctionTemplate = True
-    if fnc.find("static") != -1:
-        isFunctionStatic = True
-
-    if len(functionName.split("::")) == 1:
-        if isFunctionTemplate:
-            regex = r"Dumping {}:\nFunctionTemplateDecl.*?\n.*?\n.*?FunctionDecl.*?\n".format(functionName)
-        else:
-            regex = r"Dumping {}:\nFunctionDecl.*?\n".format(functionName)
-    else:
-        if isFunctionTemplate:
-            regex = r"Dumping {}:\nFunctionTemplateDecl.*?\n.*?\n.*?CXXMethodDecl.*?\n".format(functionName)
-        else:
-            regex = r"Dumping {}:\nCXXMethodDecl.*?\n".format(functionName)
-        isFunctionInClass = True
-
-    #Regex to get all of the functions that have the inputted name
-    retrieveAll = re.findall(regex, retrieveSystemCall, re.MULTILINE)
-
-    #Create the same syntax used in CLang to check if same prototype by making a new file with the function prototype to run CLang on it
-    f = open("temp" + source, "w")
-    #if isFunctionTemplate and isFunctionInClass:
-    if isFunctionStatic and isFunctionInClass:
-        f.write("class " + fnc.split("::")[0].strip().split(" ")[-1] + "{\n" + fnc + ";\n};" + "\n" + fnc.replace("static","").strip() + "{}")
-    elif isFunctionInClass:
-        f.write("class " + fnc.split("::")[0].strip().split(" ")[-1] + "{\n" + fnc + ";\n};" + "\n" + fnc + "{}")
-    else:
-        f.write(fnc + "{}")
-    f.close()
-    systemCall = "clang-check -ast-dump -ast-dump-filter={} {} -- 2>&1".format(functionName, "temp" + source)
-    retrieveNewCall = os.popen(systemCall).read()
-    os.remove("temp" + source)
-    if isFunctionTemplate:
-        if isFunctionInClass:
-            regexNew = r"Dumping {}:\nFunctionTemplateDecl.*?\n.*?\n.*?CXXMethodDecl.*?\n".format(functionName)
-        else:
-            regexNew = r"Dumping {}:\nFunctionTemplateDecl.*?\n.*?\n.*?FunctionDecl.*?\n".format(functionName)
-    else:
-        if isFunctionInClass:
-            regexNew = r"Dumping {}:\nCXXMethodDecl.*?\n".format(functionName)
-        else:
-            regexNew = r"Dumping {}:\nFunctionDecl.*?\n".format(functionName)
-    retrieveNew = re.findall(regexNew, retrieveNewCall, re.MULTILINE)[0]
-
-    #The line below finds all the parameters of the function inputted by the user
-    inputtedParameters = retrieveNew.split('\'')
-    lst =[]
-    for retrieveOne in retrieveAll:
-        presentParameters = retrieveOne.split('\'')
         
-        if inputtedParameters[len(inputtedParameters)-2] != presentParameters[len(presentParameters)-2]:
-            continue
-        
-        if isFunctionTemplate:
-            retrieveOne = retrieveOne.split("FunctionTemplateDecl")[1].split(':')
-        else:
-            if isFunctionInClass:
-                retrieveOne = retrieveOne.split("CXXMethodDecl")[1].split(':')
-            else:
-                retrieveOne = retrieveOne.split("FunctionDecl")[1].split(':')
-        pointer = 2
-        lst += [ [pointer, retrieveOne, isFunctionTemplate] ]
-    return lst
-
-
-
-def findLocationClass(source: str, cls: str):
-    #Checking if user input is correct and matches class prototype
-    cls.strip()
-    validClassNamePattern = r'^class [a-zA-Z_][a-zA-Z0-9_]*(\s*::\s*[a-zA-Z_][a-zA-Z0-9_]*)*$'
-    if re.match(validClassNamePattern, cls) is None:
-        print("Class name is not valid syntactically.")
-        return
+def prepareData ( source: str):
+   
+    index = clang.cindex.Index.create()
+    tu = index.parse(source)
     
-    cls = cls[5:]
-    clsFix = cls.split("::")
-    cls = ""
-    for clsPart in clsFix:
-        cls += clsPart.strip() + "::"
-    cls=cls[:len(cls)-2]
+    friendFlag = False
+    classPointer = -1
+    output = {"nodes": []}
+    for node in tu.cursor.walk_preorder():
+        if node.kind == clang.cindex.CursorKind.CLASS_DECL:
+            classPointer = 0
+            friendFlag = False
+            
+        elif node.kind == clang.cindex.CursorKind.CXX_BASE_SPECIFIER:
+            inh = node.spelling
+            output["nodes"][classPointer]["inherits_from"].append(inh)
 
-    #System call to get ast dump of anything containing the string inputted in cls (cls stands for class)
-    systemCall = "clang-check -ast-dump -ast-dump-filter={} {} -- 2>&1".format(cls, source)
-    retrieveSystemCall = os.popen(systemCall).read()
-    if retrieveSystemCall == "":
-        print("Class not found in " + source)
-        return
+        elif node.kind == clang.cindex.CursorKind.FRIEND_DECL:
+            friendFlag = True
 
-    #Filter the code using regex to keep what is important
-    regex = r"Dumping {}:\nCXXRecordDecl.*?\n".format(cls)
-    retrieveAll = re.findall(regex, retrieveSystemCall, re.MULTILINE)
+        elif friendFlag:
+            friend = node.spelling
+            if len(friend.split("class")) == 1:
+                friend = node.type.spelling.split('(')[0] + node.displayname
+            output["nodes"][classPointer]["friend_with"].append(friend)
+            friendFlag = False
 
-    clsList = []
-
-    for retrieveOne in retrieveAll:
-        checkDump = retrieveOne.split("CXXRecordDecl")
-        if checkDump[0] != ("Dumping " + cls + ":\n"):
-            continue
+        classPointer = classPointer - 1
+            
+        node_dict = {
+            "kind": str(node.kind.name),
+            "spelling": node.spelling,
+            "prototype": node.type.spelling,
+            "displayname": node.displayname,
+            "type": str(node.type),
+            "start": int(node.location.line),
+            "end": int(node.extent.end.line),
+            "semantic_parent": str(node.semantic_parent),
+            "lexical_parent": str(node.lexical_parent),
+            "mangled_name": node.mangled_name,
+            "is_const_method": node.is_const_method(),
+            "is_const_method": node.is_const_method(),
+            "is_virtual_method": node.is_virtual_method(),
+            "is_pure_virtual_method": node.is_pure_virtual_method(),
+            "is_template": node.kind == clang.cindex.CursorKind.FUNCTION_TEMPLATE,
+            "is_struct": node.kind == clang.cindex.CursorKind.STRUCT_DECL,
+            "is_class": node.kind == clang.cindex.CursorKind.CLASS_DECL,
+            "is_enum": node.kind == clang.cindex.CursorKind.ENUM_DECL,
+            "inherits_from": [],
+            "friend_with": []
+        }
+        output["nodes"].append(node_dict)
         
-        checkDump = checkDump[1].split(':')
-
-        clsList += [ [2, checkDump] ]    
-
-    #Saves informtion about the implementations of functions outside the class
-    regex = r"Dumping {}::.*?\nCXXMethodDecl.*?\n".format(cls)
-    retrieveAll = re.findall(regex, retrieveSystemCall, re.MULTILINE)
-    for retrieveOne in retrieveAll:
-        if "warning generated" in retrieveOne:
-            print("warning error from clang (code written incorrectly)")
-        else:
-            checkDump = retrieveOne.split("CXXMethodDecl")[1].split(':')
-            clsList += [ [2, checkDump] ]
-
-    #Saves information about the implementation of template functions outside the class
-    regex = r"Dumping {}::.*?:[\s\S]*CXXMethodDecl.*?\n".format(cls)
-    retrieveAll = re.findall(regex, retrieveSystemCall, re.MULTILINE)
-    for retrieveOne in retrieveAll:        
-        checkDump = retrieveOne.split("FunctionTemplateDecl")
-        if len(checkDump) != 1:
-            checkDump = checkDump[1].split(':')
-            clsList += [ [2, checkDump, True] ]
+    with open('C:\\Users\\Rund\\Desktop\\Yad\\jsonfile.json', 'w') as f:
+        json.dump(output, f, indent = 4)
         
-    #Saves informtion about the constructor and copy constructors outside the class
-    regex = r"Dumping {}::.*?:\nCXXConstructorDecl.*?\n".format(cls)
-    retrieveAll = re.findall(regex, retrieveSystemCall, re.MULTILINE)
-    for retrieveOne in retrieveAll:        
-        checkDump = retrieveOne.split("CXXConstructorDecl")[1].split(':')
-        clsList += [ [2, checkDump] ]
+    with open("jsonfile.json", "rb") as f:
+        data = orjson.loads(f.read())
+    return data
 
-    #Saves informtion about the destructors outside the class
-    regex = r"Dumping {}::.*?:\nCXXDestructorDecl.*?\n.*?\n".format(cls)
-    retrieveAll = re.findall(regex, retrieveSystemCall, re.MULTILINE)
-    for retrieveOne in retrieveAll:        
-        checkDump = retrieveOne.split("CXXDestructorDecl")[1].split(':')
-        clsList += [ [2, checkDump] ]
-
-    #Returuns informtion
-    return clsList 
-
-#This function is responsible for finding where classes or functions start and end (returns positions as a list)   
-def positions(pointer: int, retrievedAST: str, isTemplate: bool):
-    rowStart = int(retrievedAST[pointer])
-    colStart = int(retrievedAST[pointer+1].split(',')[0])
-    if isTemplate:
-        position = [rowStart, colStart, -1, -1]
-        return position
-    if retrievedAST[pointer+1].split(',')[1] == " col":
-        rowEnd = int(retrievedAST[pointer])
-        colEnd = int(retrievedAST[pointer+2].split('>')[0])
-    else:
-        rowEnd = int(retrievedAST[pointer+2])
-        colEnd = int(retrievedAST[pointer+3].split('>')[0])
-    position = [rowStart, colStart, rowEnd, colEnd, pointer]
-    return position
+def positions ( source: str, type: str, prototype: str):
+    
+    data = prepareData(source)
+    pos =[]
+    
+    if type == "class" or type == "struct":
+        pos = findLocationClass(data, prototype,0 )  
         
+    if type == "function":
+        pos = findLocationFunction( data, prototype)  
+        
+    return pos
+                    
