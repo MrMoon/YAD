29d7f538e4ecdb18ca980f904db8282b9523c898
Rund <78150340+RundRasheed@users.noreply.github.com>
2023-05-31 18:14:59 +0300

    test files

--- testfiles/destination.cpp
+++ testfiles/destination.cpp
@@ -1,25 +1,8 @@
-// #include <iosatrem>
-// using namespace std;
 
-
-int main(){
-	cout <<"Destination";
-	book b1;
-	b1.sum(5);
-	return 0;
-}
-
-class A{
-	template <typename T> T myMax(T &x, T y);
-};
-class B{
- friend class A;
-};
 class Animal {
 	public:
-	void eat() {
-	cout << "Animal is eating." << endl;
-	}
+	static void eat();
+	void print() const;
 	private:
 		int x;
 	public:
@@ -31,13 +14,685 @@ class Animal {
 	int sum(int y, int z);
 	
 };
-	int sum(int y, int z);
 int Animal :: sum(int y, int z){
 	return y+z;
 }
-class Dog : public Animal {
-	public:
-	void bark() {
-	cout << "Dog is barking." << endl;
-	}
+
+//class Animal {
+//	public:
+//	static void eat();
+//	void print() const;
+//	private:
+//		int x;
+//	public:
+//	int y;
+//	int sum(int x){
+//		return x+5;
+//	}
+//	friend class A;
+////	int sum(int y, int z);
+//	
+//};
+//int Animal :: sum(int y, int z){
+//	return y+z;
+//}
+
+class C1class: Aclass{
+    private:
+    int i;
+    int j;
+    int C1test(){
+        return 0;
+    }
 };
+class C3class: C1class{
+    private:
+    int i;
+    int j;
+    int C3test(){
+        return 0;
+    }
+};
+
+//class C1class: Aclass{
+//    private:
+//    int i;
+//    int j;
+//    int C1test(){
+//        return 0;
+//    }
+//};
+//class C3class: C1class{
+//    private:
+//    int i;
+//    int j;
+//    int C3test(){
+//        return 0;
+//    }
+//};
+
+//class C1class: Aclass{
+//    private:
+//    int i;
+//    int j;
+//    int C1test(){
+//        return 0;
+//    }
+//};
+//class C3class: C1class{
+//    private:
+//    int i;
+//    int j;
+//    int C3test(){
+//        return 0;
+//    }
+//};
+
+//class C1class: Aclass{
+//    private:
+//    int i;
+//    int j;
+//    int C1test(){
+//        return 0;
+//    }
+//};
+//class C3class: C1class{
+//    private:
+//    int i;
+//    int j;
+//    int C3test(){
+//        return 0;
+//    }
+//};
+
+//class C1class: Aclass{
+//    private:
+//    int i;
+//    int j;
+//    int C1test(){
+//        return 0;
+//    }
+//};
+//class C3class: C1class{
+//    private:
+//    int i;
+//    int j;
+//    int C3test(){
+//        return 0;
+//    }
+//};
+
+//class C1class: Aclass{
+//    private:
+//    int i;
+//    int j;
+//    int C1test(){
+//        return 0;
+//    }
+//};
+//class C3class: C1class{
+//    private:
+//    int i;
+//    int j;
+//    int C3test(){
+//        return 0;
+//    }
+//};
+
+//class C1class: Aclass{
+//    private:
+//    int i;
+//    int j;
+//    int C1test(){
+//        return 0;
+//    }
+//};
+//class C3class: C1class{
+//    private:
+//    int i;
+//    int j;
+//    int C3test(){
+//        return 0;
+//    }
+//};
+
+//class C1class: Aclass{
+//    private:
+//    int i;
+//    int j;
+//    int C1test(){
+//        return 0;
+//    }
+//};
+//class C3class: C1class{
+//    private:
+//    int i;
+//    int j;
+//    int C3test(){
+//        return 0;
+//    }
+//};
+
+//class C1class: Aclass{
+//    private:
+//    int i;
+//    int j;
+//    int C1test(){
+//        return 0;
+//    }
+//};
+//class C3class: C1class{
+//    private:
+//    int i;
+//    int j;
+//    int C3test(){
+//        return 0;
+//    }
+//};
+
+//class C1class: Aclass{
+//    private:
+//    int i;
+//    int j;
+//    int C1test(){
+//        return 0;
+//    }
+//};
+//class C3class: C1class{
+//    private:
+//    int i;
+//    int j;
+//    int C3test(){
+//        return 0;
+//    }
+//};
+
+//class C1class: Aclass{
+//    private:
+//    int i;
+//    int j;
+//    int C1test(){
+//        return 0;
+//    }
+//};
+//class C3class: C1class{
+//    private:
+//    int i;
+//    int j;
+//    int C3test(){
+//        return 0;
+//    }
+//};
+
+//class C1class: Aclass{
+//    private:
+//    int i;
+//    int j;
+//    int C1test(){
+//        return 0;
+//    }
+//};
+//class C3class: C1class{
+//    private:
+//    int i;
+//    int j;
+//    int C3test(){
+//        return 0;
+//    }
+//};
+
+//class C1class: Aclass{
+//    private:
+//    int i;
+//    int j;
+//    int C1test(){
+//        return 0;
+//    }
+//};
+//class C3class: C1class{
+//    private:
+//    int i;
+//    int j;
+//    int C3test(){
+//        return 0;
+//    }
+//};
+
+//class C1class: Aclass{
+//    private:
+//    int i;
+//    int j;
+//    int C1test(){
+//        return 0;
+//    }
+//};
+//class C3class: C1class{
+//    private:
+//    int i;
+//    int j;
+//    int C3test(){
+//        return 0;
+//    }
+//};
+
+//class C1class: Aclass{
+//    private:
+//    int i;
+//    int j;
+//    int C1test(){
+//        return 0;
+//    }
+//};
+//class C3class: C1class{
+//    private:
+//    int i;
+//    int j;
+//    int C3test(){
+//        return 0;
+//    }
+//};
+
+//class C1class: Aclass{
+//    private:
+//    int i;
+//    int j;
+//    int C1test(){
+//        return 0;
+//    }
+//};
+//class C3class: C1class{
+//    private:
+//    int i;
+//    int j;
+//    int C3test(){
+//        return 0;
+//    }
+//};
+
+//class C1class: Aclass{
+//    private:
+//    int i;
+//    int j;
+//    int C1test(){
+//        return 0;
+//    }
+//};
+//class C3class: C1class{
+//    private:
+//    int i;
+//    int j;
+//    int C3test(){
+//        return 0;
+//    }
+//};
+
+//class C1class: Aclass{
+//    private:
+//    int i;
+//    int j;
+//    int C1test(){
+//        return 0;
+//    }
+//};
+//class C3class: C1class{
+//    private:
+//    int i;
+//    int j;
+//    int C3test(){
+//        return 0;
+//    }
+//};
+
+//class C1class: Aclass{
+//    private:
+//    int i;
+//    int j;
+//    int C1test(){
+//        return 0;
+//    }
+//};
+//class C3class: C1class{
+//    private:
+//    int i;
+//    int j;
+//    int C3test(){
+//        return 0;
+//    }
+//};
+
+//class C1class: Aclass{
+//    private:
+//    int i;
+//    int j;
+//    int C1test(){
+//        return 0;
+//    }
+//};
+//class C3class: C1class{
+//    private:
+//    int i;
+//    int j;
+//    int C3test(){
+//        return 0;
+//    }
+//};
+
+class Fclass{
+    private:
+    int i;
+    int j;
+    
+//    Fclass();
+    Fclass(Fclass &a);
+    ~Fclass();
+private: ;
+};
+
+//class Fclass{
+//    private:
+//    int i;
+//    int j;
+//    
+////    Fclass();
+//    Fclass(Fclass &a);
+//    ~Fclass();
+//private: ;
+//};
+
+//class Fclass{
+//    private:
+//    int i;
+//    int j;
+//    
+////    Fclass();
+//    Fclass(Fclass &a);
+//    ~Fclass();
+//private: ;
+//};
+
+//class Fclass{
+//    private:
+//    int i;
+//    int j;
+//    
+////    Fclass();
+//    Fclass(Fclass &a);
+//    ~Fclass();
+//private: ;
+//};
+
+//class Fclass{
+//    private:
+//    int i;
+//    int j;
+//    
+////    Fclass();
+//    Fclass(Fclass &a);
+//    ~Fclass();
+//private: ;
+//};
+
+//class Fclass{
+//    private:
+//    int i;
+//    int j;
+//    
+////    Fclass();
+//    Fclass(Fclass &a);
+//    ~Fclass();
+//private: ;
+//};
+
+//class Fclass{
+//    private:
+//    int i;
+//    int j;
+//    
+////    Fclass();
+//    Fclass(Fclass &a);
+//    ~Fclass();
+//private: ;
+//};
+
+//class Fclass{
+//    private:
+//    int i;
+//    int j;
+//    
+////    Fclass();
+//    Fclass(Fclass &a);
+//    ~Fclass();
+//private: ;
+//};
+
+//class Fclass{
+//    private:
+//    int i;
+//    int j;
+//    
+////    Fclass();
+//    Fclass(Fclass &a);
+//    ~Fclass();
+//private: ;
+//};
+
+//class Fclass{
+//    private:
+//    int i;
+//    int j;
+//    
+////    Fclass();
+//    Fclass(Fclass &a);
+//    ~Fclass();
+//private: ;
+//};
+
+//class Fclass{
+//    private:
+//    int i;
+//    int j;
+//    
+////    Fclass();
+//    Fclass(Fclass &a);
+//    ~Fclass();
+//private: ;
+//};
+
+//class Fclass{
+//    private:
+//    int i;
+//    int j;
+//    
+////    Fclass();
+//    Fclass(Fclass &a);
+//    ~Fclass();
+//private: ;
+//};
+
+//class Fclass{
+//    private:
+//    int i;
+//    int j;
+//    
+////    Fclass();
+//    Fclass(Fclass &a);
+//    ~Fclass();
+//private: ;
+//};
+
+//class Fclass{
+//    private:
+//    int i;
+//    int j;
+//    
+////    Fclass();
+//    Fclass(Fclass &a);
+//    ~Fclass();
+//private: ;
+//};
+
+//class Fclass{
+//    private:
+//    int i;
+//    int j;
+//    
+////    Fclass();
+//    Fclass(Fclass &a);
+//    ~Fclass();
+//private: ;
+//};
+
+//class Fclass{
+//    private:
+//    int i;
+//    int j;
+//    
+////    Fclass();
+//    Fclass(Fclass &a);
+//    ~Fclass();
+//private: ;
+//};
+
+//class Fclass{
+//    private:
+//    int i;
+//    int j;
+//    
+////    Fclass();
+//    Fclass(Fclass &a);
+//    ~Fclass();
+//private: ;
+//};
+
+//class Fclass{
+//    private:
+//    int i;
+//    int j;
+//    
+////    Fclass();
+//    Fclass(Fclass &a);
+//    ~Fclass();
+//private: ;
+//};
+
+//class Fclass{
+//    private:
+//    int i;
+//    int j;
+//    
+////    Fclass();
+//    Fclass(Fclass &a);
+//    ~Fclass();
+//private: ;
+//};
+
+//class Fclass{
+//    private:
+//    int i;
+//    int j;
+//    
+////    Fclass();
+//    Fclass(Fclass &a);
+//    ~Fclass();
+//private: ;
+//};
+
+//Fclass::Fclass(){
+//    i = 0;
+//    j = 1;
+//}
+
+//Fclass::Fclass(){
+//    i = 0;
+//    j = 1;
+//}
+
+//Fclass::Fclass(){
+//    i = 0;
+//    j = 1;
+//}
+
+//Fclass::Fclass(){
+//    i = 0;
+//    j = 1;
+//}
+
+//Fclass::Fclass(){
+//    i = 0;
+//    j = 1;
+//}
+
+//Fclass::Fclass(){
+//    i = 0;
+//    j = 1;
+//}
+
+//Fclass::Fclass(){
+//    i = 0;
+//    j = 1;
+//}
+
+//Fclass::Fclass(){
+//    i = 0;
+//    j = 1;
+//}
+
+//Fclass::Fclass(){
+//    i = 0;
+//    j = 1;
+//}
+
+//Fclass::Fclass(){
+//    i = 0;
+//    j = 1;
+//}
+
+//Fclass::Fclass(){
+//    i = 0;
+//    j = 1;
+//}
+
+//Fclass::Fclass(){
+//    i = 0;
+//    j = 1;
+//}
+
+//Fclass::Fclass(){
+//    i = 0;
+//    j = 1;
+//}
+
+//Fclass::Fclass(){
+//    i = 0;
+//    j = 1;
+//}
+
+//Fclass::Fclass(){
+//    i = 0;
+//    j = 1;
+//}
+
+//Fclass::Fclass(){
+//    i = 0;
+//    j = 1;
+//}
+
+//Fclass::Fclass(){
+//    i = 0;
+//    j = 1;
+//}
+
+//Fclass::Fclass(){
+//    i = 0;
+//    j = 1;
+//}
+
+//Fclass::Fclass(){
+//    i = 0;
+//    j = 1;
+//}
+
+Fclass::Fclass(){
+    i = 0;
+    j = 1;
+}
